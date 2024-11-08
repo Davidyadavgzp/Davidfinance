7e89328b86f490e01af1872e8c97e3ab3ef4ca14
Davidyadavgzp <davidyadavgzp73@gmail.com>
2024-11-08 13:16:35 +0000

    change

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Define both '/' and '/home' as routes for the home page
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+@app.route('/home')
+def index():
+    return render_template('index.html')
 
-if __name__ == "__main__": 
-    app.run(host='0.0.0.0', debug=True)
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
